e08db08989b8b1c4e91f118e6113bbbe9d048fc1
Tony Malghem <tmalghem@gmail.com>
2014-12-15 21:25:13 +0100

    Recap en fin de partie
    Ajout du code pour afficher le récap de l'histoire du jouer une fois
    qu'il a atteint la fin de la partie

--- app/src/main/java/com/projet/tony/tx/LostLetter/fin.java
+++ app/src/main/java/com/projet/tony/tx/LostLetter/fin.java
@@ -4,27 +4,79 @@ import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.JsonReader;
+import android.util.JsonToken;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
 import com.projet.tony.tx.R;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
 public class fin extends Activity {
+
+    private final static File IMG_DIR = new File(Environment.getExternalStorageDirectory().getPath()+"/OCR/images");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fin);
+        ScrollView scrollView = (ScrollView) findViewById(R.id.scrollViewFin);
+        LinearLayout globalLayout = (LinearLayout) findViewById(R.id.linearLayoutFin);
 
+        try {
+            JSONObject jsonHistoire = parseHistoire(getAssets().open("LostLetters.JSON"));
+            TextView tv=(TextView) findViewById(R.id.titre);
+            Typeface font = Typeface.createFromAsset(getAssets(), "KQ.ttf");
+            tv.setTypeface(font);
+            tv.setTextSize(25);
+            tv.setText("Fin ! Voici votre histoire...");
+            TextView title=(TextView) findViewById(R.id.titleHist);
+            title.setTypeface(font);
+            title.setTextSize(18);
+            title.setText(jsonHistoire.get("titre").toString());
+            TextView intro=(TextView) findViewById(R.id.introHist);
+            intro.setTypeface(font);
+            intro.setTextSize(14);
+            intro.setText(jsonHistoire.get("histoire").toString());
 
-        TextView tv=(TextView) findViewById(R.id.titre);
-        Typeface font = Typeface.createFromAsset(getAssets(), "KQ.ttf");
-        tv.setTypeface(font);
-        tv.setTextSize(40);
-        tv.setText("Fin");
+            int nbOfImages = IMG_DIR.listFiles().length;
+            String questionTab = jsonHistoire.get("listQuestions").toString();
+            questionTab = questionTab.replace("[","");
+            questionTab = questionTab.replace("]","");
+            String[] arrayQuestions = questionTab.split(",");
+            for(int i=0;i<nbOfImages;i++) {
+                TextView textView = new TextView(this);
+                textView.setTypeface(font);
+                textView.setTextSize(14);
+                textView.setText(arrayQuestions[i]);
+                textView.setLayoutParams(new ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.WRAP_CONTENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT
+                ));
+                globalLayout.addView(textView,i);
+            }
+        }
+        catch (Exception ioe) {
+            ioe.printStackTrace();
+        }
 
 
         final Button monbutt = (Button) findViewById(R.id.button);
@@ -39,6 +91,65 @@ public class fin extends Activity {
                 startActivity(intent);
             }
         });
+        setContentView(scrollView);
+    }
+
+    public JSONObject parseHistoire(InputStream inputStream) throws IOException, JSONException {
+        JsonReader jsonReader = new JsonReader(new InputStreamReader(inputStream));
+        try {
+            return readHistory(jsonReader);
+        }
+        finally {
+            jsonReader.close();
+        }
+    }
+
+    public JSONObject readHistory(JsonReader jsonReader) throws IOException, JSONException {
+        JSONObject returnObject = new JSONObject();
+        jsonReader.beginObject();
+        while(jsonReader.hasNext()) {
+            String name = jsonReader.nextName();
+            if(name.equals("titre") && jsonReader.peek()!= JsonToken.NULL) {
+                returnObject.put("titre",jsonReader.nextString());
+            }
+            else if(name.equals("histoire") && jsonReader.peek()!= JsonToken.NULL) {
+                returnObject.put("histoire", jsonReader.nextString());
+            }
+            else if(name.equals("enigme")) {
+                returnObject.put("listQuestions",readEnigmes(jsonReader));
+            }
+            else {
+                jsonReader.skipValue();
+            }
+        }
+        jsonReader.endObject();
+        return returnObject;
+    }
+
+    public ArrayList<String> readEnigmes(JsonReader jsonReader) throws IOException, JSONException {
+        ArrayList<String> retEnigmes = new ArrayList<String>();
+        jsonReader.beginArray();
+        while(jsonReader.hasNext()) {
+            retEnigmes.add(readEnigme(jsonReader));
+        }
+        jsonReader.endArray();
+        return retEnigmes;
+    }
+
+    public String readEnigme(JsonReader jsonReader) throws IOException {
+        String ret = null;
+        jsonReader.beginObject();
+        while(jsonReader.hasNext()) {
+            String name = jsonReader.nextName();
+            if(name.equals("question") && jsonReader.peek() != JsonToken.NULL) {
+                ret = jsonReader.nextString();
+            }
+            else {
+                jsonReader.skipValue();
+            }
+        }
+        jsonReader.endObject();
+        return ret;
     }
 
 
